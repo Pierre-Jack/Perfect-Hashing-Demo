059f0041dc1830ed1d97d3b468d42db837875dbf
NiraIbrahem <125669902+NiraIbrahem@users.noreply.github.com>
2024-04-22 02:01:18 +0200

    Update Perfect_Hashing_NSqaure.java

--- Perfect_Hashing/src/Perfect_Hashing_NSqaure.java
+++ Perfect_Hashing/src/Perfect_Hashing_NSqaure.java
@@ -1,205 +1,143 @@
 import java.util.Arrays;
 
-public class Perfect_Hashing_NSqaure {
-    private int sizeOfMaxInput;
-    private int sizeOfHashTable;
-    private int[] hashTable;
-    private boolean[][] func ;
-    private int b ;
-    private int u ;
-    Universal_Hash_Family s = new Universal_Hash_Family(3,32); //Universal_Hash_Family s = new Universal_Hash_Family(b
-    private int currentInputSize;
-    private boolean[] isOccupied;
-    private Hashing h ;
+public class Perfect_Hashing_NSquare {
+    private int sizeOfMaxInput;     /* size of max input , increasing dynamically with insertion operations */
+    private int sizeOfHashTable;    /* size of hash table = M = N^2 */
+    private int[] hashTable;        /* hash table */
+    private boolean[][] func ;      /* array of hash func */
+    private int b ;                 /* M = 2 ^ b  , so b = log (M) to the base 2 */
+    private int u ;                 /* length of hashed element , 32bit for integer , and so on ...... */
+    private Universal_Hash_Family s ;   /* object from the universal hash family */
+    private Hashing h ;                 /* object from the Hashing class */
+    private int currentInputSize;       /* keep track of number of elements inserted */
+    private boolean[] isOccupied;       /* keep track for indexes that is used in hashTable */
+    private double load_factor ;        /* load factor for percentage of filling hashTable */
 
-    public Perfect_Hashing_NSqaure(int sizeOfMaxInput) {
-        this.sizeOfMaxInput = sizeOfMaxInput;
-        this.sizeOfHashTable = sizeOfMaxInput*sizeOfMaxInput;
-        this.func = s.hash_function();
-        this.h = new Hashing(func);
+    public Perfect_Hashing_NSquare() {
+        this.sizeOfMaxInput = 8;        /* initial Max input */
+        this.sizeOfHashTable = this.sizeOfMaxInput*this.sizeOfMaxInput;
+        this.b = (int) (Math.log(this.sizeOfHashTable) /Math.log(2) ) ;
+        this.u = 32 ;
+        this.s = new Universal_Hash_Family(this.b,this.u) ; /* creating universal hash family */
+        this.func = this.s.hash_function();
+        this.h = new Hashing(this.func);
         this.currentInputSize = 0;
-        this.hashTable = new int[sizeOfHashTable];
-        this.isOccupied = new boolean[sizeOfHashTable];
+        this.hashTable = new int[this.sizeOfHashTable];
+        this.isOccupied = new boolean[this.sizeOfHashTable];
+        Arrays.fill(this.isOccupied , false) ;
+        this.load_factor = .75 ;
     }
 
     public void insert(int element){
-        currentInputSize++;
-        double loadFactor = 0.75;
-        if((double) currentInputSize /sizeOfHashTable > loadFactor){
-            rehash(element);
-        }
-        else{
-            int index = h.hash_code(element);
-            if(!isOccupied[index]){
-                hashTable[index] = element;
-                isOccupied[index] = true;
-            }
-            else{
-                rebuildIfCollision(element);
-            }
+        int index = this.h.hash_code(element) ;
 
+        System.out.println("elemnt = " + element + " , index = " + index ) ;
+
+        if(this.isOccupied[index] && this.hashTable[index] == element){     /* error is the element is already inserted */
+            System.out.println("The element is inserted already") ;
         }
-        System.out.println("element:" + element + " index:" + h.hash_code(element) );
+        else {
+            this.currentInputSize++;
+            if ((double) this.currentInputSize / this.sizeOfMaxInput > this.load_factor) {
+
+                System.out.println("the size will increase") ;
+
+                this.rehash(element , true);       /* increasing size of hashTable */
+            } else {
+                if (!this.isOccupied[index]) {      /* no corner case , inserted normally */
+                    this.hashTable[index] = element;
+                    this.isOccupied[index] = true;
+
+                    System.out.println("no problem , will insert") ;
+
+                } else {
+
+                    System.out.println("collision , will re-build") ;
+
+                    this.rehash(element , false);       /* collision , re-build and choosing new hash function */
+                }
+            }
+        }
     }
+    private void rehash(int element , boolean load_factor_broken){
+        int[] temp = this.hashTable.clone();        /* copy hashTable and occupied function */
+        boolean [] occupied_temp = this.isOccupied.clone() ;
 
-    public void rebuildIfCollision(int element){
-        func = s.hash_function();
-        h = new Hashing(func);
-        Arrays.fill(isOccupied, false);
-        int[] temp = hashTable.clone(); // Copy the existing hash table
-        //hashTable = new int[sizeOfHashTable];
-        for(int i = 0; i < temp.length; i++){
-            int index = h.hash_code(element);
-            if(!isOccupied[index]){
-                hashTable[index] = element;
-                isOccupied[index] = true;
-            }
-            else{
-                rebuildIfCollision(temp[i]);
-            }
+        if(load_factor_broken) {
+            this.sizeOfMaxInput = this.sizeOfMaxInput * 2;        /* increasing max elements am=nd size of Table */
+            this.sizeOfHashTable = this.sizeOfMaxInput * this.sizeOfMaxInput;
+            this.b = (int) (Math.log(this.sizeOfHashTable) / Math.log(2));
+
+            this.s = new Universal_Hash_Family(this.b, this.u);       /* generating new hash family */
         }
-        insert(element); // Insert the new element
-    }
 
+        boolean finished ;  /* for determining if the rehash operation is done */
+
+        do{
+            finished = true ;
+            this.hashTable = new int[this.sizeOfHashTable];     /* clear hashTable and isOccupied array */
+            this.isOccupied = new boolean[this.sizeOfHashTable];
+            Arrays.fill(this.isOccupied, false);
+
+            this.func = this.s.hash_function();         /* generate new hash function */
+            this.h = new Hashing(this.func);
+
+            int index = this.h.hash_code(element) ;     /* insert element value in the hashable */
+            this.hashTable[index] = element ;
+            this.isOccupied[index] = true ;
+
+            System.out.println("elemnt = " + element + " , index = " + index ) ;
+
+            for(int i=0 ; i<temp.length ; i++){         /* insert elements that are in the hashTable and if there is collision repeat with new function */
+                if(occupied_temp[i]){
+                    index = this.h.hash_code(temp[i]) ;
+
+                    System.out.println("elemnt = " + temp[i] + " , index = " + index ) ;
+
+                    if(!this.isOccupied[index]){
+                        this.hashTable[index] = temp[i] ;
+                        this.isOccupied[index] = true ;
+                    }
+                    else {
+                        finished = false ;          /* collision , create new hash function */
+                        System.out.println("collision , will re-build") ;
+                        break ;
+                    }
+                }
+            }
+        }while(!finished) ;
+    }
     public void delete(int element){
-        currentInputSize--;
-        int index = h.hash_code(element);
-        if(hashTable[index] == element){
-            hashTable[index] = 0;
-            isOccupied[index] = false;
+        int index = this.h.hash_code(element);
+        if(this.isOccupied[index] && this.hashTable[index] == element){
+            this.currentInputSize--;
+            this.isOccupied[index] = false;
         }
         else{
-            //element not found
-            System.out.println("Element not found");
+            System.out.println("Element not found");    /* error , element not found */
         }
     }
 
     public boolean search(int element){
-        int index = h.hash_code(element);
-        return hashTable[index] == element;
+        int index = this.h.hash_code(element);
+        return this.isOccupied[index];
     }
-
-    public void rehash(int element){
-//        int[] temp = hashTable;
-//        sizeOfMaxInput = sizeOfMaxInput*2;
-//        sizeOfHashTable = sizeOfMaxInput*sizeOfMaxInput;
-//        hashTable = new int[sizeOfHashTable];
-//        isOccupied = new boolean[sizeOfHashTable];
-//        Arrays.fill(isOccupied, false);
-//        for(int i = 0; i < temp.length; i++){
-//                insert(temp[i]);
-//        }
-        //insert(element);
-        int[] temp = hashTable.clone();
-        sizeOfMaxInput = sizeOfMaxInput*2;
-        sizeOfHashTable = sizeOfMaxInput*sizeOfMaxInput;
-        hashTable = new int[sizeOfHashTable];
-        isOccupied = new boolean[sizeOfHashTable];
-        Arrays.fill(isOccupied, false);
-        for(int i = 0; i < temp.length; i++){
-            int index = h.hash_code(element);
-            if(!isOccupied[index]){
-                hashTable[index] = element;
-                isOccupied[index] = true;
-            }
-            else{
-                rebuildIfCollision(temp[i]);
-            }
-        }
-        insert(element);
-    }
-
     public int getCurrentInputSize() {
         return currentInputSize;
     }
-
-    public void setCurrentInputSize(int currentInputSize) {
-        this.currentInputSize = currentInputSize;
-    }
-
     public boolean[][] getFunc() {
         return func;
     }
-
-    public void setFunc(boolean[][] func) {
-        this.func = func;
-    }
-
     public int getB() {
         return b;
     }
-
-    public void setB(int b) {
-        this.b = b;
-    }
-
     public int getU() {
         return u;
     }
-
-    public void setU(int u) {
-        this.u = u;
-    }
-
     public int getSizeOfMaxInput() {
         return sizeOfMaxInput;
     }
-
-    public void setSizeOfMaxInput(int sizeOfMaxInput) {
-        this.sizeOfMaxInput = sizeOfMaxInput;
-    }
-
     public int getSizeOfHashTable() {
         return sizeOfHashTable;
     }
-
-    public void setSizeOfHashTable(int sizeOfHashTable) {
-        this.sizeOfHashTable = sizeOfHashTable;
-    }
-
-    public static void main(String[] args){
-        Perfect_Hashing_NSqaure perfectHashing = new Perfect_Hashing_NSqaure(10);
-        perfectHashing.insert(10);
-     //   System.out.println(perfectHashing.search(10));
-        perfectHashing.insert(20);
-        //System.out.println(perfectHashing.search(10));
-        perfectHashing.insert(30);
-       // System.out.println(perfectHashing.search(10));
-        perfectHashing.insert(40);
-        //System.out.println(perfectHashing.search(10));
-        perfectHashing.insert(50);
-        //System.out.println(perfectHashing.search(10));
-        perfectHashing.insert(60);
-       // System.out.println(perfectHashing.search(10));
-        perfectHashing.insert(70);
-       // System.out.println(perfectHashing.search(10));
-        perfectHashing.insert(80);
-       // System.out.println(perfectHashing.search(10));
-        perfectHashing.insert(90);
-      //  System.out.println(perfectHashing.search(10));
-        perfectHashing.insert(100);
-////        System.out.println(perfectHashing.search(10));
-//        perfectHashing.insert(110);
-////        System.out.println(perfectHashing.search(10));
-//        perfectHashing.insert(120);
-////        System.out.println(perfectHashing.search(10));
-//        perfectHashing.insert(130);
-////        System.out.println(perfectHashing.search(10));
-//        perfectHashing.insert(140);
-////        System.out.println(perfectHashing.search(10));
-//        perfectHashing.insert(150);
-        System.out.println(perfectHashing.search(10));
-
-//        perfectHashing.delete(90);
-//
-//        System.out.println(perfectHashing.search(90));
-//        System.out.println(perfectHashing.getSizeOfHashTable());
-//        for(int i = 0; i < perfectHashing.getSizeOfHashTable(); i++){
-//            System.out.println(perfectHashing.hashTable[i]);
-//        }
-
-
-
-    }
-
 }
