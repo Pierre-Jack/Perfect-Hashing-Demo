398a989ed3c0a0f1e65ccf8c4b86ae5909aca0fc
PierreJack <pierrejacques2002@gmail.com>
2024-04-23 16:28:41 +0200

    Update UniversalHasher.java

--- Perfect_Hashing/src/UniversalHasher.java
+++ Perfect_Hashing/src/UniversalHasher.java
@@ -1,16 +1,20 @@
 public class UniversalHasher<T> extends Hasher<T> {
     private int m;
+    private int b;
     private Universal_Hash_Family<T> family;
     private Hashing<T> hashing;
 
     public UniversalHasher(int m) {
         super(m);
+        b = 0;
+        while((1 << b) < m) b += 1;
+
     }
 
     @Override
     int hash_code(T key) {
         if(family == null || hashing == null) {
-            family = new Universal_Hash_Family<T>(key, (int) Math.log(m));
+            family = new Universal_Hash_Family<T>(key, b);
             hashing = new Hashing<T>(family.hash_function());
         }
         return hashing.hash_code(key);
