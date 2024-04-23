930a9bf649829cacbbdebeb03b1e1cd32953aa97
NiraIbrahem <125669902+NiraIbrahem@users.noreply.github.com>
2024-04-23 05:07:54 +0200

    Update Main.java

--- Perfect_Hashing/src/Main.java
+++ Perfect_Hashing/src/Main.java
@@ -3,16 +3,15 @@ import java.util.Scanner;
 
 public class Main {
 
-//    public void print(boolean [][] arr){
-//        for(int k=0 ; k<arr.length ; k++){
-//            for(int j=0 ; j<arr[0].length ; j++){
-//                System.out.print(Boolean.compare(arr[k][j],false)+" ") ;
-//            }
-//            System.out.println() ;
-//        }
-//        System.out.println("*******************************************") ;
-//    }
-
+/*    public void print(boolean [][] arr){
+        for(int k=0 ; k<arr.length ; k++){
+            for(int j=0 ; j<arr[0].length ; j++){
+                System.out.print(Boolean.compare(arr[k][j],false)+" ") ;
+            }
+            System.out.println() ;
+        }
+        System.out.println("*******************************************") ;
+    }*/
 
     public static void main(String[] args) {
 
@@ -45,61 +44,132 @@ public class Main {
 
 
 
-//        Main x = new Main() ;
-//        //int M = 8;
-//        int b = 3;
-//        int u = 32;
-//        Universal_Hash_Family s = new Universal_Hash_Family(b,u);
-//        boolean arr [][] = s.hash_function();
-//        x.print(arr);
-////        Universal_Hash_Family s2 = new Universal_Hash_Family(b,5);
-////        boolean arr2 [][] = s2.hash_function();
-////        x.print(arr2);
-////        for(int i=0 ; i<10 ; i++){
-////            boolean arr [][] = s.hash_function();
-////            x.print(arr);
-////        }
-//
-//        Hashing h = new Hashing(arr);
-//        int [] values = new int [] {10 , 18 , 25 , 39 , 65 , 82 , 111 , -100 } ;
-//        for(int i= 0 ; i< values.length ; i++){
-//            int index = h.hash_code(values[i]);
-//            x.print(h.vector) ;
-//            System.out.println(index);
-//            System.out.println("-------------------------------------------");
-//        }
-        Perfect_Hashing_NSquare perfectHashing = new Perfect_Hashing_NSquare();
-        perfectHashing.insert(10);
-        System.out.println(perfectHashing.search(10));
-        perfectHashing.insert(20);
-        System.out.println(perfectHashing.search(20));
-        perfectHashing.insert(30);
-        System.out.println(perfectHashing.search(30));
-        perfectHashing.insert(40);
-        System.out.println(perfectHashing.search(40));
-        perfectHashing.insert(50);
-        System.out.println(perfectHashing.search(50));
-        perfectHashing.insert(60);
-        System.out.println(perfectHashing.search(60));
-        perfectHashing.insert(70);
-        System.out.println(perfectHashing.search(70));
-        perfectHashing.insert(80);
-        System.out.println(perfectHashing.search(80));
-        perfectHashing.insert(90);
-        System.out.println(perfectHashing.search(90));
-        perfectHashing.insert(100);
-        System.out.println(perfectHashing.search(100));
+/*
+        Main x = new Main() ;
+        int b = 3;
+        int u = 64;
+        Universal_Hash_Family s = new Universal_Hash_Family(b,u);
+        boolean arr [][] = s.hash_function();
+        x.print(arr);
+*/
 
 
-        perfectHashing.delete(90);
-        System.out.println(perfectHashing.search(90));
+/*
+        Hashing<Integer> h = new Hashing<>(arr);
+        int [] values = new int [] {10 , 18 , 25 , 39 , 65 , 82 , 111 , -100 , -2147483648 } ;
+        for(int i= 0 ; i< values.length ; i++){
+            int index = h.hash_code(values[i]);
+            x.print(h.vector) ;
+            System.out.println(index);
+            System.out.println("-------------------------------------------");
+        }
 
-        perfectHashing.delete(90);
-        perfectHashing.insert(80) ;
+*/
+
+/*
+        Hashing<Byte> h = new Hashing<>(arr);
+        byte [] values = new byte [] {10 , 18 , 25 , 39 , 65 , 82 , 111 , -100 , -128 } ;
+        for(int i= 0 ; i< values.length ; i++){
+            int index = h.hash_code(values[i]);
+            x.print(h.vector) ;
+            System.out.println(index);
+            System.out.println("-------------------------------------------");
+        }
+
+*/
 
 
-        System.out.println(perfectHashing.getSizeOfHashTable());
-        System.out.println(perfectHashing.getCurrentInputSize());
+/*
+        Hashing<Short> h = new Hashing<>(arr);
+        short [] values = new short [] {10 , 18 , 25 , 39 , 65 , 82 , 111 , -100 , -32768} ;
+        for(int i= 0 ; i< values.length ; i++){
+            int index = h.hash_code(values[i]);
+            x.print(h.vector) ;
+            System.out.println(index);
+            System.out.println("-------------------------------------------");
+        }
+
+*/
+
+
+/*
+        Hashing<Long> h = new Hashing<>(arr);
+        long [] values = new long [] {1l , 16l , 128l , 1024l , 32768l ,  2147483648l , 9223372036854775807l , -9223372036854775808l} ;
+        for(int i= 0 ; i< values.length ; i++){
+            int index = h.hash_code(values[i]);
+            x.print(h.vector) ;
+            System.out.println(index);
+            System.out.println("-------------------------------------------");
+        }
+*/
+
+
+
+/*
+        Hashing<Character> h = new Hashing<>(arr);
+        char [] values = new char [] {'H' , 'E' , 'L' , 'L' , 'O' , ' '} ;
+        */
+/* H = 72
+           E = 69
+           L = 76
+           L = 76
+           O = 79
+           SPACE = 32
+         *//*
+
+
+        for(int i= 0 ; i< values.length ; i++){
+            int index = h.hash_code(values[i]);
+            x.print(h.vector) ;
+            System.out.println(index);
+            System.out.println("-------------------------------------------");
+        }
+
+*/
+
+
+/*
+        Hashing<String> h = new Hashing<>(arr);
+
+            int index = h.hash_code("HelloWorld");
+            */
+/*
+             H = 72
+             e = 101
+             l = 108
+             l = 108
+             o = 111
+             W = 87
+             o = 111
+             r = 114
+             l = 108
+             d = 100
+             *//*
+
+            x.print(h.vector) ;
+            System.out.println(index);
+            System.out.println("-------------------------------------------");
+*/
+
+
+/*
+            Hashing<String> h = new Hashing<>(arr);
+
+            int index = h.hash_code("Hello");
+            */
+/*
+             H = 72
+             e = 101
+             l = 108
+             l = 108
+             o = 111
+             *//*
+
+            x.print(h.vector) ;
+            System.out.println(index);
+            System.out.println("-------------------------------------------");
+
+*/
 
     }
 }
