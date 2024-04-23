bfa273207c2d8de811215fceb6df8a3a8f70e264
NiraIbrahem <125669902+NiraIbrahem@users.noreply.github.com>
2024-04-23 05:04:51 +0200

    Update Hashing.java

--- Perfect_Hashing/src/Hashing.java
+++ Perfect_Hashing/src/Hashing.java
@@ -1,23 +1,28 @@
 import java.lang.* ;
-
+import java.util.* ;
     /*
         this class calculates the hash code of given integer according to hash function defined to it
     */
-    public class Hashing {
+    public class Hashing<T> {
         private boolean func[][] ;
         private int b ;
         private int u ;
 
-        /* construcor for intializing hash function and its dims */
+        /*
+        public boolean [][] vector ;
+        */
+
+        /* constructor for initializing hash function and its dims */
         Hashing(boolean [][] arr){
             this.func = arr ;
             this.b = arr.length ;
             this.u = arr[0].length ;
+
         }
 
         /* function for calculating hash code for given integer */
-        public int hash_code(int element){
-            boolean[][] vector = this.evaluate_X_vector(element) ;
+        public int hash_code(T key){
+            boolean[][] vector = this.evaluate_X_vector(key) ;
             return this.evaluate_index_in_hash_table(vector) ;
         }
 
@@ -27,11 +32,47 @@ import java.lang.* ;
         true refer to 1
         false refer to 0
         */
-        private boolean[][] evaluate_X_vector(int element){
+        private boolean[][] evaluate_X_vector(T key){
             boolean [][] vector = new boolean [this.u][1];
-            for (int i=0 ; i<this.u ; i++){
-                vector[i][0] = this.getBit(element , i) ;
+
+            if(key instanceof Integer) {
+                for (int i = 0; i < this.u; i++) {
+                    vector[i][0] = this.getBit((int) key, i);
+                }
+            } else if (key instanceof Short) {
+                for (int i = 0; i < this.u; i++) {
+                    vector[i][0] = this.getBit(Integer.valueOf((short)key), i);
+                }
+            } else if (key instanceof Byte) {
+                for (int i = 0; i < this.u; i++) {
+                    vector[i][0] = this.getBit(Integer.valueOf((byte)key), i);
+                }
+            } else if (key instanceof Character) {
+                for (int i = 0; i < this.u; i++) {
+                    vector[i][0] = this.getBit(Integer.valueOf((char)key), i);
+                }
+            }else if (key instanceof String) {
+                String s = (String) key ;
+                for (int i = 0; i < s.length(); i++) {
+                    for(int j=0 ; j<7 ; j++){
+                        vector[7*i+j][0] = this.getBit(Integer.valueOf(s.charAt(i)), j);
+                    }
+                }
+            } else if (key instanceof Long) {
+                for (int i = 0; i < this.u; i++) {
+                    if(i==this.u-1){
+                        vector[i][0] = (long)key == ((long)key | (long)Math.pow(-2 , this.u-1));
+                    }
+                    else {
+                        vector[i][0] = (long)key == ((long)key | (long)Math.pow(2 , i)) ;
+                    }
+                }
+            } else{
+                throw new RuntimeException("couldn't hash these types");
             }
+            /*
+            this.vector = vector ;
+            */
             return vector ;
         }
 
@@ -47,6 +88,7 @@ import java.lang.* ;
                 x =  number | (int)Math.pow(2,position);
             return x==number ;
         }
+
 
     /*  Boolean.compare(x ,false) return 1 if x == true
                                   return 0 if x == false
@@ -66,5 +108,17 @@ import java.lang.* ;
                 dummy = 0 ;
             }
             return index ;
+        }
+
+        public boolean[][] getFunc() {
+            return this.func;
+        }
+
+        public int getB() {
+            return this.b;
+        }
+
+        public int getU() {
+            return this.u;
         }
     }
